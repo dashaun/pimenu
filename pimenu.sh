7841f9a7be851c7616e96e8ecbb9a02a9463379a
dashaun <dashaun.carter@mobilecapitalgroup.com>
2016-11-11 13:39:03 -0600

    update selectors

--- pimenu.sh
+++ pimenu.sh
@@ -1,34 +1,17 @@
 #!/bin/bash
 key=$(echo "$*" | awk 'NF>1{print $NF}')
 case $key in
-    Camera)
-        ps aux | grep -ie gphoto2 | awk '{print $2}' | xargs kill -9
-        cd
-        if [ ! -d "PHOTOS" ]; then
-            mkdir PHOTOS
-        fi
-        cd PHOTOS
-        gphoto2 --get-all-files --new --filename "%Y%m%d-%H%M%S.%C"
+    Music)
+	echo "Music $key" >> /home/pi/pimenu.log
     ;;
-    USB)
-        cd
-        if [ ! -d "PHOTOS" ]; then
-            mkdir PHOTOS
-        fi
-        USB_MOUNT_POINT=$(find /media/ -mindepth 1 -maxdepth 1 -not -empty -type d | \
-                          sed 's/ /\\ /g')
-        exiftool -r -d /home/pi/PHOTOS/%Y%m%d-%H%M%S.%%e '-FileName<DateTimeOriginal' \
-                $USB_MOUNT_POINT >> /home/pi/transfer.log
+    Video)
+	echo "Video $key" >> /home/pi/pimenu.log
     ;;
-    Backup)
-        cd
-        if [ ! -d "PHOTOS" ]; then
-            exit 1
-        fi
-        USB_MOUNT_POINT=$(find /media/ -mindepth 1 -maxdepth 1 -type d | sed 's/ /\\ /g')
-        eval rsync -avhz /home/pi/PHOTOS/ $USB_MOUNT_POINT/ --log-file=/home/pi/backup.log
+    Video1)
+	echo "Video1 $key" >> /home/pi/pimenu.log
     ;;
     '')
+	echo "Empty $key" >> /home/pi/pimenu.log
         ps aux | grep -ie pimenu | awk '{print $2}' | xargs kill -9
     ;;
 esac
